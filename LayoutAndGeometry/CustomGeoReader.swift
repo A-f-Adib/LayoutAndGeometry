fb7b8e31f10d24cefd4a51b310ce01da54921002
A.F. Adib <a.f.adib36@gmail.com>
2024-10-03 23:56:27 +0600

    modified

--- LayoutAndGeometry/CustomGeoReader.swift
+++ LayoutAndGeometry/CustomGeoReader.swift
@@ -16,13 +16,16 @@ struct CustomGeoReader: View {
     }
 
     var body: some View {
+        
         ScrollView(.horizontal, showsIndicators: false) {
             HStack {
                 ForEach(0..<20) { indx in
                     
                     GeometryReader { geometry in
                         RoundedRectangle(cornerRadius: 20)
-                            .rotation3DEffect(Angle(degrees: 10), axis: (x: 0.0, y: 1.0, z: 0.0))
+                            .rotation3DEffect(
+                                Angle(degrees: getPercentage(geo: geometry) * 40),
+                                axis: (x: 0.0, y: 1.0, z: 0.0))
                     }
                     .frame(width: 300, height: 250)
                     .padding()
